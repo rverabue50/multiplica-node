008193e0d2697bb056a98a0408a05fcc7c4c62f1
Ricardo <rverabue50@hotmail.com>
2020-12-11 10:28:09 -0500

    Commit inicial

---
+++ app.js
@@ -0,0 +1,48 @@
+// destructarion
+
+const argv = require('./config/yargs').argv;
+//const colors = require('colors');
+const colors = require('colors/safe');
+
+
+const { crearArchivo, listarTabla } = require('./multiplicar/multiplicar');
+
+let comando = argv._[0];
+
+switch (comando) {
+    case 'listar':
+        listarTabla(argv.base, argv.limite);
+        break;
+
+    case 'crear':
+        crearArchivo(argv.base, argv.limite)
+            .then(archivo => console.log(`Archivo creado: `, colors.green(archivo)))
+            .catch(e => console.log(e))
+        break;
+
+    default:
+        console.log('comando no reconocido')
+
+}
+
+//console.log(argv)
+//let base = '5';
+
+//console.log(process.argv);
+
+//let argv = process.argv;
+//let parametro = argv[2];
+//let base = parametro.split('=')[1];
+//console.log(base);
+
+//let argv2 = process.argv;
+
+//console.log(argv.base);
+//console.log(argv2);
+//console.log(argv.limite);
+
+
+
+// crearArchivo(base)
+//     .then(archivo => console.log(`Archivo creado: ${ archivo}`))
+//     .catch(e => console.log(e))
